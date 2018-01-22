2c4d35c66a6f12d2cda012daffae23953496a56a
revannth.github.io <revannth1997@gmail.com>
2018-01-22 20:57:16 +0530

    Landing Page added

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date(2018,0,25,10,0,0))
+		//timestamp : (new Date()).getTime() + 08*10*60*60*1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
