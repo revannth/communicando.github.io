2e4e2e2f6956dbff137ecce6eb00f4e4be33cc67
revannth.github.io <revannth1997@gmail.com>
2018-02-08 21:08:07 +0530

    SSL certificate added

--- app/views.py
+++ app/views.py
@@ -29,3 +29,8 @@ def ourteam():
 @app.route('/seniorcoords')
 def ourteamsen():
 	return render_template('Our_team2.html')
+
+## SSL
+@app.route('/.well-known/acme-challenge/X2Pkr_LnjjzoSyRBs21vOhGdJjR_UfnsC0yW7e_20e0')
+def ssl():
+	return 'X2Pkr_LnjjzoSyRBs21vOhGdJjR_UfnsC0yW7e_20e0.cv6qm5th8axNzQZckSVY86wzXXt95VRO0ZVL2Uy0T1E'
