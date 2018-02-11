723dbfc2f2f4e28c642d4dc9edf048f3066cc38a
revannth.github.io <revannth1997@gmail.com>
2018-02-11 13:04:06 +0530

    Registration form added

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class Participant(db.Model):
 	email = db.Column(db.String(128),unique=True,nullable=False)
 	college = db.Column(db.String(240),nullable=False)
 	mobile = db.Column(db.String(15))
-	event = db.Column(db.String(11),nullable=False)
+	event = db.Column(db.String(50),nullable=False)
 	participant_id = db.Column(db.Integer,primary_key=True)
 
 
