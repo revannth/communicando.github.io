edd452b8e7400c803f63a63e9cdcd951e216dd83
Revannth <revannth1997@gmail.com>
2019-01-30 22:26:47 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,15 +3,6 @@ from flask import Flask, render_template, redirect, url_for
 from flask_mail import Mail,  Message
 
 app = Flask(__name__)
-app.config.update(
-    MAIL_SERVER='smtp.googlemail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME = 'revannth1997@gmail.com',
-    MAIL_PASSWORD = 'r3va99th1nn7'
-)
-
-mail = Mail(app)
 
 @app.route('/send-mail/')
 def send_mail():
